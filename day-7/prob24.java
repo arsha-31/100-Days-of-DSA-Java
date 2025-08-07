f30c05be724d3b7b8c593f75c2bda33f8a621812
arsha-31 <arshaasgar31@gmail.com>
2025-08-07 18:37:12 +0530

    modified java prob24

--- day-7/prob24.java
+++ day-7/prob24.java
@@ -14,7 +14,6 @@ public class prob24 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        // Upper part
         for(int i=1; i<=n; i++) {
             for(int j=1; j<=i; j++) {
                 System.out.print("*");
@@ -27,7 +26,6 @@ public class prob24 {
             }
             System.out.println();
         }
-        // Lower part
         for(int i=n; i>=1; i--) {
             for(int j=1; j<=i; j++) {
                 System.out.print("*");
